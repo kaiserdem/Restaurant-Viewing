15ada8648f74b060e4b00873b3ecdbc93dc2187f
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-24 23:46:11 +0300

    created main screen with layout

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Kaiserdem on 24.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
